ad9316fcebfd3e46efe95642406b5e540a7195df
javier.vazquez <javier.vazquez@rationaltouch.com>
2019-05-19 12:37:17 +0200

    Refactor to battle time battle system

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -1,66 +1,86 @@
-import { Delays, greeter } from '../src/main';
 import troop from './fixtures/troop';
 
-interface Troop {
+interface iTroop {
   hp: number;
   damage: number;
   name: string;
+  hitSpeed: number;
+  currentHp: number;
+  nextAttack: number;
+  updateNextAttack(time: number): void;
+}
+
+class Troop implements iTroop {
+  hp: number;
+  damage: number;
+  name: string;
+  hitSpeed: number;
+  currentHp: number;
+  nextAttack: number;
+  // tslint:disable-next-line:typedef
+  constructor({ hp, damage, name, hitSpeed }) {
+    this.nextAttack = this.hitSpeed = hitSpeed;
+    this.currentHp = this.hp = hp;
+    this.damage = damage;
+    this.name = name;
+  }
+
+  public updateNextAttack(time: number): void {
+    const nextAttack = parseFloat((this.nextAttack - time).toFixed(1));
+    this.nextAttack = nextAttack === 0.0 ? this.hitSpeed : nextAttack;
+  }
 }
 class Battle {
-  private foreign: Troop;
-  constructor(private local: Troop) {}
-  public vs(foreign: Troop): Troop {
+  private foreign: iTroop;
+  private time: number = 0;
+
+  constructor(private local: iTroop) {}
+
+  public vs(foreign: iTroop): iTroop {
     this.foreign = foreign;
     return this.fight();
   }
-  private fight(): Troop {
-    while (this.local.hp > 0) {
-      this.foreign.hp -= this.local.damage;
-      if (this.foreign.hp < 0) break;
-      this.local.hp -= this.foreign.damage;
+
+  private fight(): iTroop {
+    while (this.local.hp > 0 && this.foreign.hp > 0) {
+      const isLocalAttack = this.local.nextAttack < this.foreign.nextAttack;
+      if (isLocalAttack) {
+        this.foreign.hp -= this.local.damage;
+        this.time += this.local.nextAttack;
+        this.foreign.updateNextAttack(this.local.nextAttack);
+        this.local.updateNextAttack(this.local.nextAttack);
+        console.log('Local Attack', this.time);
+      } else {
+        this.local.hp -= this.foreign.damage;
+        this.time += this.foreign.nextAttack;
+        this.local.updateNextAttack(this.foreign.nextAttack);
+        this.foreign.updateNextAttack(this.foreign.nextAttack);
+        console.log('Foreign Attack', this.time);
+      }
     }
     return this.local.hp > 0 ? this.local : this.foreign;
   }
 }
+
 describe('Troops', () => {
   describe('One Card Battle', () => {
     describe('Individual troop', () => {
       it('Melee Battle - Wins First', () => {
-        const result = new Battle(troop.miniPekka).vs(troop.knight);
-        expect(result.name).toBe(troop.miniPekka.name);
+        const miniPekka = new Troop(troop.miniPekka);
+        const knight = new Troop(troop.knight);
+
+        const result = new Battle(miniPekka).vs(knight);
+
+        expect(result.name).toBe(miniPekka.name);
       });
       it('Melee Battle - Wins Second', () => {
-        const result = new Battle(troop.knight).vs(troop.miniPekka);
+        const miniPekka = new Troop(troop.miniPekka);
+        const knight = new Troop(troop.knight);
+
+        const result = new Battle(knight).vs(miniPekka);
+
         expect(result.name).toBe(troop.miniPekka.name);
       });
     });
-  });
-});
-describe('greeter function', () => {
-  // Read more about fake timers: http://facebook.github.io/jest/docs/en/timer-mocks.html#content
-  jest.useFakeTimers();
-
-  const name: string = 'John';
-  let hello: string;
-
-  // Act before assertions
-  beforeAll(async () => {
-    const p: Promise<string> = greeter(name);
-    jest.runOnlyPendingTimers();
-    hello = await p;
-  });
-
-  // Assert if setTimeout was called properly
-  it('delays the greeting by 2 seconds', () => {
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(
-      expect.any(Function),
-      Delays.Long,
-    );
-  });
-
-  // Assert greeter result
-  it('greets a user with `Hello, {name}` message', () => {
-    expect(hello).toBe(`Hello, ${name}`);
   });
 });
